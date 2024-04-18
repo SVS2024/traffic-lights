4170c0f57019594db1e29b3b6fc355a736be8d55
Sefrgey <voronins77@icloud.com>
2024-04-18 22:11:55 +0300

    osnova

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var yellow: UIView!
+    @IBOutlet var green: UIView!
+    @IBOutlet var red: UIView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        yellow.layer.cornerRadius = 75
+        green.layer.cornerRadius = 75
+        red.layer.cornerRadius = 75
+        button.layer.cornerRadius = 10
+        
+        yellow.alpha = 0.3
+        green.alpha = 0.3
+        red.alpha = 0.3
+        
+        
         // Do any additional setup after loading the view.
     }
 
